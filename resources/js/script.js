0848147515ac8bd1fc4cc6880526cdd90e306926
Dnavarro88 <42564706+Dnavarro88@users.noreply.github.com>
2021-07-12 11:07:19 -0700

    Added animations on scroll

--- resources/js/script.js
+++ resources/js/script.js
@@ -26,4 +26,21 @@ $(document).ready(function(){
     $root.animate({scrollTop: $( $.attr(this, 'href') ).offset().top}, 1000);
     return false;
     });
+
+    /* Animations on scroll */
+    $('.js--wp-1').waypoint((direction) => {
+        $('.js--wp-1').addClass('animate__animated animate__fadeIn');}, { offset: '50%', }
+      );
+    
+    $('.js--wp-2').waypoint((direction) => {
+        $('.js--wp-2').addClass('animate__animated animate__fadeInUp');}, { offset: '50%', }
+    );  
+
+    $('.js--wp-3').waypoint((direction) => {
+        $('.js--wp-3').addClass('animate__animated animate__fadeIn');}, { offset: '50%', }
+    );  
+
+    $('.js--wp-4').waypoint((direction) => {
+        $('.js--wp-4').addClass('animate__animated animate__pulse');}, { offset: '50%', }
+    );  
 });
